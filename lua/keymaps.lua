54c7ac5115e3a22f740814075035ba170d82d2f8
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-03-24 02:43:04 +0100

    feat: map diagnostics keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -34,6 +34,13 @@ vim.keymap.set("n", "<Space>j", "<C-w>j", { noremap = true })
 vim.keymap.set("n", "<Space>k", "<C-w>k", { noremap = true })
 vim.keymap.set("n", "<Space>l", "<C-w>l", { noremap = true })
 
+-- Diagnostic keymaps
+-- TODO: remap this section
+vim.keymap.set('n', 'gp', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' }) --TODO: fix telescope file search
+vim.keymap.set('n', 'gn', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
