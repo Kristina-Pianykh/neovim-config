f2f6fba0225f74a162cf967315e82543a73817dd
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-03-27 13:57:33 +0100

    chore: add parsers for hcl and tf

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -3,7 +3,7 @@
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = {'c', 'lua', 'vim', 'python' },
+      ensure_installed = {'c', 'lua', 'vim', 'python', 'hcl', 'terraform' },
       -- Autoinstall languages that are not installed
       auto_install = false,
       highlight = {
