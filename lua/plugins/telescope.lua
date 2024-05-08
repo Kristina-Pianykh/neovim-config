3ed4d46b8b6159f3a6126b423822bff593a6f644
Kristina Pianykh <kristinavrnrus@gmail.com>
2024-05-09 01:00:37 +0200

    chore: rg config is now handled by home-manager

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-  { -- Fuzzy Finder (files, lsp, etc)
+  {
     "nvim-telescope/telescope.nvim",
     event = "VimEnter",
     branch = "0.1.x",
@@ -24,36 +24,7 @@ return {
       { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
     },
     config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
       require("telescope").setup({
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
         defaults = {
           -- People say it's slow
           -- rather use rg?
@@ -69,25 +40,6 @@ return {
           find_files = {
             find_command = {
               "rg",
-              "--no-ignore",
-              "--hidden",
-              "-g",
-              "!**/.git/**",
-              "-g",
-              "!**/.venv",
-              "-g",
-              "!**/dist",
-              "-g",
-              "!**/build",
-              "-g",
-              "!**/target",
-              "-g",
-              "!**/*cache*",
-              "-g",
-              "!**/__pycache__",
-              "-g",
-              "!**/*.pyc",
-              "--unrestricted",
               "--files",
             },
           },
@@ -109,7 +61,6 @@ return {
       pcall(require("telescope").load_extension, "fzf")
       pcall(require("telescope").load_extension, "ui-select")
 
-      -- See `:help telescope.builtin`
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<Space>p", builtin.find_files, {})
       vim.keymap.set("n", "<Space>g", builtin.live_grep, {})
@@ -193,15 +144,6 @@ return {
           })
         )
       end, { desc = "[/] Fuzzily search in current buffer" })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      -- vim.keymap.set('n', '<leader>s/', function()
-      --   builtin.live_grep {
-      --     grep_open_files = true,
-      --     prompt_title = 'Live Grep in Open Files',
-      --   }
-      -- end, { desc = '[S]earch [/] in Open Files' })
 
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set("n", "<leader>sn", function()
