b3666839b0b903da9fd65a2dbe3cd9f06ed2ce34
Kristina Pianykh <kristinavrnrus@gmail.com>
2025-11-18 16:13:25 +0100

    lsp: add configs for nix, tf, htlm, json, golangcilint, gopls

--- lua/plugins/lsp-new.lua
+++ lua/plugins/lsp-new.lua
@@ -325,9 +325,83 @@ return {
         },
       })
 
+      -- Go lint
+      local custom_lint = vim.fn.getenv("FLINK_LINT")
+      -- print(custom_lint)
+      local command
+      if custom_lint ~= vim.NIL then
+        if custom_lint == "true" then
+          local golangci_lint_path = vim.fn.getcwd() .. "/build/goflink-golint"
+          command = {
+            golangci_lint_path,
+            "run",
+            "--output.json.path=stdout",
+            "--show-stats=false",
+          }
+        end
+      else
+        command = {
+          "golangci-lint",
+          "run",
+          "--no-config",
+          "run",
+          "--output.json.path=stdout",
+          "--show-stats=false",
+        }
+      end
+      -- print(vim.inspect(command)) -- debug
+      lspconfig("golangci_lint_ls", {
+        init_options = {
+          command = command,
+        },
+      })
+
+      lspconfig("gopls", {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*.go",
+        callback = function()
+          local params = vim.lsp.util.make_range_params()
+          params.context = { only = { "source.organizeImports" } }
+          -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+          -- machine and codebase, you may want longer. Add an additional
+          -- argument after params if you find that you have to write the file
+          -- twice for changes to be saved.
+          -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+          local result =
+            vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+          for cid, res in pairs(result or {}) do
+            for _, r in pairs(res.result or {}) do
+              if r.edit then
+                local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding
+                  or "utf-16"
+                vim.lsp.util.apply_workspace_edit(r.edit, enc)
+              end
+            end
+          end
+          vim.lsp.buf.format({ async = false })
+        end,
+      })
+
       vim.lsp.enable({
+        "nixd",
+        "tflint",
+        "terraformls",
+        "jsonls",
+        "html",
         "lua_ls",
         "gopls",
+        "golangci_lint_ls",
         "pyright",
         "golangci_lint_ls",
         "yamlls",
